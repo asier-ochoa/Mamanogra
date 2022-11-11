e1b0d36f6e277250b8d5b9800e6a3b2829d9d82a
asier-ochoa <78745593+asier-ochoa@users.noreply.github.com>
2022-11-11 02:52:24 -0400

    keys are invalid, calm down bud

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
 from music.commands import bot
 
-bot.run('ODg4NTE3NjMyOTY0MzA5MDYy.YUT2hw.SFpgnCRKvue1fBc-h1xemet-l90')
-
-#ODg4NTE3NjMyOTY0MzA5MDYy.YUT2hw.SFpgnCRKvue1fBc-h1xemet-l90
-#OTUxNTk3MjEwMDkxNTI0MTM2.Yipx9g.Ra-d4B3QjLbZmWCF470dAnJ8nkM
+bot.run('')
