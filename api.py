f265daa1b61f6d3384039af1372729a7482aca46
Asier Ochoa <asierxs05@gmail.com>
2023-02-07 21:11:02 -0400

    changed ctrl list to a dict

--- api.py
+++ api.py
@@ -3,7 +3,7 @@ import asyncio
 from flask.blueprints import Blueprint
 from flask import request
 import music.player as player
-from music.commands import music_ctrl_list
+from music.commands import controllers
 
 bp = Blueprint('bp', __name__)
 
@@ -11,7 +11,7 @@ bp = Blueprint('bp', __name__)
 @bp.post("/play")
 def do():
     body = request.get_json(force=True)
-    ctrl = [x for x in music_ctrl_list if x[1].name == "Punishment Zone"][0]
+    ctrl = [x for x in controllers if x[1].name == "Punishment Zone"][0]
     caller = [x for x in ctrl[1].members if x.name == "Smug Twingo"][0]
     ctrl = ctrl[0]
     asyncio.ensure_future(ctrl.play_url(body['url'], caller), loop=ctrl.bot.loop)
