cb5bb716602a66d255b9c6bed4c3ef1cb33d85ca
Asier <asier@asier.asier>
2023-02-28 00:05:54 -0400

    Added skipping command

--- discord_server_defaults.py
+++ discord_server_defaults.py
@@ -34,10 +34,10 @@ async def play_url_command(msg: Message, srv: Server, yt_id: str = None):
 async def play_query_command(msg: Message, srv: Server, query: str = None):
     queries = query.split('|')
     if len(queries) == 1:
-        print(f"Info: {msg.author.name}#{msg.author.discriminator} playing single query \"{query}\"")
+        print(f"Info: {msg.author.name}#{msg.author.discriminator} queuing single query \"{query}\"")
         await srv.music_player.play(msg.author, generate_youtube_song(f"ytsearch:{queries[0]}"))
     else:
-        print(f"Info: {msg.author.name}#{msg.author.discriminator} playing multi query \"{query}\"")
+        print(f"Info: {msg.author.name}#{msg.author.discriminator} queuing multi query \"{query}\"")
         for q in queries:
             await srv.music_player.play(msg.author, generate_youtube_song(f"ytsearch:{q}"))
     pass
@@ -47,11 +47,19 @@ async def play_playlist_command(msg: Message, srv: Server, yt_id: str = None):
     pass
 
 
+async def skip_command(msg: Message, srv: Server):
+    async with srv.music_player.voice_client_lock:
+        if srv.music_player.voice_client is None:
+            return
+    print(f"Info: {msg.author.name}#{msg.author.discriminator} skipped forward in {srv.disc_guild.name}")
+    srv.music_player.voice_client.stop()
+
 def get_defaults(prefix: str):
     commands = [
         (fr"\{prefix}(?:info|i)", info_command),
         (fr"\{prefix}(?:p|play) https:\/\/(?:(?:www\.youtube\.com\/.*?watch\?v=([\w\d]*).*)|(?:youtu\.be\/([\w\d\-\_]+)))", play_url_command),
         (fr"\{prefix}(?:p|play) ([^|]+(?:\|(?:[^|]+))*)", play_query_command),
-        (fr"\{prefix}(?:pl|playlist) https:\/\/www.youtube.com\/.*?list=([\w\d]*).*", play_playlist_command)
+        (fr"\{prefix}(?:pl|playlist) https:\/\/www.youtube.com\/.*?list=([\w\d]*).*", play_playlist_command),
+        (fr"\{prefix}(?:s|skip)", skip_command)
     ]
     return [Command(*x) for x in commands]
